b80c92ecc43844327e06d329c9f95ce5a0050e63
enoquelights <84310653+enoquelights@users.noreply.github.com>
2025-06-19 10:19:00 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -8,7 +8,7 @@ from app.logging.config import config
 rebar = Rebar()
 v1_registry = rebar.create_handler_registry(
     prefix='/v1',
-    swagger_path='/apidocs',
+    spec_path='/apidocs',
     swagger_ui_path='/apidocs-ui',
     swagger_generator=SwaggerV3Generator(title="Face Recognition",
                                          description="Face Recognition REST Service using DLIB")
